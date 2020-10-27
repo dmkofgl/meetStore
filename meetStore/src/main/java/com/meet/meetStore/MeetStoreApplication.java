5bf80fa8cb373293c703a4d14a71c0725398d493
Uladzislau.Barkou <dmkofgl@gmail.com>
2020-10-27 17:55:24 +0300

    init

---
+++ meetStore/src/main/java/com/meet/meetStore/MeetStoreApplication.java
@@ -0,0 +1,13 @@
+package com.meet.meetStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetStoreApplication.class, args);
+	}
+
+}
---
+++ meetStore/src/test/java/com/meet/meetStore/MeetStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meet.meetStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
